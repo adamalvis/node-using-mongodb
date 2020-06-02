3b5015f2bafcedde7ba9d09a8c8feef3cc05c9c6
Adam Alvis <adam.alvis@gmail.com>
2020-06-02 13:50:44 -0400

    completed module 4

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -13,5 +13,24 @@ export const addnewProduct = (req, res) => {
 
     return res.json(Product);
   });
-}
+};
 
+export const getProducts = (req, res) => {
+  Product.find({}, (err, Product) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    return res.json(Product);
+  });
+};
+
+export const getProductWithID = (req, res) => {
+  Product.findById(req.params.ProductID, (err, Product) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    return res.json(Product);
+  });
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
-import { addnewProduct } from "../controllers/controllers";
+import { addnewProduct, getProducts, getProductWithID } from "../controllers/controllers";
 
 function routes(app) {
   app.route('/products')
-    .post(addnewProduct);
+    .post(addnewProduct)
+    .get(getProducts);
+
+  app.route('/products/:ProductID')
+    .get(getProductWithID);
 }
 
 export default routes;
