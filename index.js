4504565c05c9ef03e52d94c97e8ff604f4224691
Adam Alvis <adam.alvis@gmail.com>
2020-06-02 13:30:03 -0400

    completed module 2

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
 
 const app = express();
 const PORT = 4000;
@@ -9,9 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
 
 app.get('/', (req, res) =>
-    res.send(`Store server running on port ${PORT}`)
+  res.send(`Store server running on port ${PORT}`)
 );
 
 app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
+  console.log(`Your server is running on port ${PORT}`)
 );
